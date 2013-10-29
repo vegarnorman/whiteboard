1ee1c0a51dbffbac34d68ab627c2898f6fb2a9f1
Vegar Norman <vegar@theinternetloves.me>
2013-10-29 09:43:30 +0100

    Opprettet Database.class.php
    Klasse som brukes for databasetilkoblinger.

---
+++ application/classes/Database.class.php
@@ -0,0 +1,32 @@
+<?php
+	
+	require "Database.auth.class.php";
+	require "Database.settings.php";
+
+	class Database extends mysqli {
+
+		public function __construct() {
+			global $db1, $db2, $db3, $db4;
+			$auth = new DatabaseAuth(DATABASE_AUTH_SALT);
+			$host = $auth->decrypt($db1);
+			$user = $auth->decrypt($db2);
+			$pass = $auth->decrypt($db3);
+			$use  = $auth->decrypt($db4);
+
+			parent::__construct($host, $user, $pass, $use);
+
+		}
+
+		public function connected() {
+			if ($this->connect_error) {
+				return false;
+			}
+
+			else {
+				return true;
+			}
+		}
+
+	}
+
+?>
