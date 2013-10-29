3f4af96b75396b4e314f2149bc986586df7f76f9
Vegar Norman <vegar@theinternetloves.me>
2013-10-29 09:47:15 +0100

    Opprettet Database.settings.php
    Inneholder databaseinformasjon i kryptert format.

---
+++ application/classes/Database.settings.php
@@ -0,0 +1,7 @@
+<?php
+	define('DATABASE_AUTH_SALT', '_RYy9ll{jk4V2m9v}c>9^?ejl=.tO!{XVnXh3WfdIBE.`#x[0p3jqKi6JSD2{uWL');
+	$db1 = 'bWrB+0cOEKhVJpC5fWGUAXHnmBEEqoWCtQqfHqnMR+0=';
+	$db2 = 'QE7IKoU1Pk7/gkigSo3FTmZIZ2miR5vvWjSNIRTgPD8=';
+	$db3 = 'yOrCnl26nvyMkMhDYq6T54KmSyYDSZd3O3134O6F4Uc=';
+	$db4 = '4DaG/I4htCR0tyPLEtfOTIXRL9cpdW73g9/Ys4uaKtc=';
+?>
