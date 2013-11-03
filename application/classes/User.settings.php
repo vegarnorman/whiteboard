6a74e9cc4f81b815257238f74eb1e996ffd61531
Vegar Norman <vegar@theinternetloves.me>
2013-11-03 20:27:35 +0100

    Opprettet User.settings.php
    Inneholder hashinginformasjon.

---
+++ application/classes/User.settings.php
@@ -0,0 +1,27 @@
+<?php
+
+	$s = [
+		0 => 'YPbjw=uByX`kEQOH/MKoXE/a.@aj]XcL[dYuMO(){p+X(Y*j`nn$|c|=*^IW5I{$YPbjw=uByX`kEQOH/MKoXE/a.@aj]XcL[dYuMO(){p+X(Y*j`nn$|c|=*^IW5I{$',
+		1 => 'Rh,}o)0dh4^>pjIZHJ?E5ZhT&;0<hfRjpFwpF!;%9o>-wWp`[qC[LrO2k51BfRpYRh,}o)0dh4^>pjIZHJ?E5ZhT&;0<hfRjpFwpF!;%9o>-wWp`[qC[LrO2k51BfRpY',
+		2 => 'rjtMZ`+W%Eea^u_<mJ?@t|3APLEhgw**Q}3*|NXw6<}W>}(1J?5oy5Or,0h_u6!FrjtMZ`+W%Eea^u_<mJ?@t|3APLEhgw**Q}3*|NXw6<}W>}(1J?5oy5Or,0h_u6!F',
+		3 => 'EuW;^fy4+2(797HqT9T9Sh|<;^NG381Gys*-oY+j:>l+=B{e(-PD/xl W[BqAj3QEuW;^fy4+2(797HqT9T9Sh|<;^NG381Gys*-oY+j:>l+=B{e(-PD/xl W[BqAj3Q',
+		4 => '3j+|)g>yX-X~nvHN:T#F9D];XD2-- 6u]|0!netBPEP=.cn/Vn0G5BV(:d:vkj?K3j+|)g>yX-X~nvHN:T#F9D];XD2-- 6u]|0!netBPEP=.cn/Vn0G5BV(:d:vkj?K',
+		5 => 's$g1$f6]f4+mT*2f8oX%^Bc?p:LS]+c?WP@`k3+S|~P=e6@J5[Q-tyH2#=nyj&;?s$g1$f6]f4+mT*2f8oX%^Bc?p:LS]+c?WP@`k3+S|~P=e6@J5[Q-tyH2#=nyj&;?',
+		6 => 'hJlJF|So)&C:N%EZ<>lH}kudx#_a8(&3eP[Ybw;5N#yVPH[B|pi%i3HL/Oa~L#M]hJlJF|So)&C:N%EZ<>lH}kudx#_a8(&3eP[Ybw;5N#yVPH[B|pi%i3HL/Oa~L#M]',
+		7 => 'cRS;3:Bx4Srh9Qd7Jm[zD.nENo<iv+-&>a}U/0IbPW/+6u^*jF-XaMi/hfI{^i<*cRS;3:Bx4Srh9Qd7Jm[zD.nENo<iv+-&>a}U/0IbPW/+6u^*jF-XaMi/hfI{^i<*',
+		8 => 'm+;jI3`x~9+39<i-1e?B)Jj?O&gWt<o[hB)m2M,~Y/k:q9fCdIMc_C!dv8QNFBb8m+;jI3`x~9+39<i-1e?B)Jj?O&gWt<o[hB)m2M,~Y/k:q9fCdIMc_C!dv8QNFBb8',
+		9 => 'UI;9MPb]885/ne$Bw`FgXEV62t--i/7n *|({#)P6H4@Htlik4;1;1|oSn&zqvrGUI;9MPb]885/ne$Bw`FgXEV62t--i/7n *|({#)P6H4@Htlik4;1;1|oSn&zqvrG',
+		10 => '1J_t!T@@qywLh8qv<H|Q5f@+S3YMK||-J+[Yp_URD0UE86c6Bn1?*OWa]6`gI.q$1J_t!T@@qywLh8qv<H|Q5f@+S3YMK||-J+[Yp_URD0UE86c6Bn1?*OWa]6`gI.q$',
+		11 => '+k&2h70FwMh-gL HXOBQj1obV:Z[@Y*vn39xOlrb^(8$_o0ZbemR$fZEbs!M,7 !+k&2h70FwMh-gL HXOBQj1obV:Z[@Y*vn39xOlrb^(8$_o0ZbemR$fZEbs!M,7 !',
+		12 => '?nDb+vV&iZ|OP|Ii{~<:Yr[DQ3+!t$$6!]TLv/XG*5n|OnE>w|:+44wnB$u/*+ITnDb+vV&iZ|OP|Ii{~<:Yr[DQ3+!t$$6!]TLv/XG*5n|OnE>w|:+44wnB$u/*+IT',
+		13 => 'c`#4.#k7?D(/~YJ|kMW<a>A|>b/FIC9#%&uU=mj~bB@F;B%m DRI)L+jB&V=A8`@c`#4.#k7?D(/~YJ|kMW<a>A|>b/FIC9#%&uU=mj~bB@F;B%m DRI)L+jB&V=A8`@',
+		14 => '^_kQz:4oLUiA!aG8/J1-;07ku84i$lU[u*-Jzv( YvGkA{W+@/6qjUeJkQe6!aVV^_kQz:4oLUiA!aG8/J1-;07ku84i$lU[u*-Jzv( YvGkA{W+@/6qjUeJkQe6!aVV',
+		15 => 'k+:doX_qK4TE^DvT%23b{h]LsC9TD.f|emPiUm&Kf,1cBBt:_eU-WLbS-,BnC3!Fk+:doX_qK4TE^DvT%23b{h]LsC9TD.f|emPiUm&Kf,1cBBt:_eU-WLbS-,BnC3!F',
+		16 => 'o@JkCb,79@&lfg n[OkJ8p,;o%W(AH|#ni}+-K}~I3sI;1F^FI(o2HD4zWU-j;+bo@JkCb,79@&lfg n[OkJ8p,;o%W(AH|#ni}+-K}~I3sI;1F^FI(o2HD4zWU-j;+b',
+		17 => 'LZW3yj1rN|/<ws(t5|dl{|,_6&U-YTiP9i}YpUR--O8(p+Z4{HwG+|9VawzCe!5ILZW3yj1rN|/<ws(t5|dl{|,_6&U-YTiP9i}YpUR--O8(p+Z4{HwG+|9VawzCe!5I',
+		18 => ',|yG<)xUgr69;/o*|J8S*U{}z#-_`IG7 F^.i9O;W(i;=P#+[--(h/XA?w)X3?Z8,|yG<)xUgr69;/o*|J8S*U{}z#-_`IG7 F^.i9O;W(i;=P#+[--(h/XA?w)X3?Z8',
+		19 => 'x4[$HJ))cm}kL0Vg*uT9+?4]GXtx=7WVZ<Ol8RPQQ6,^2pP=z$&QuwrUk.+}}RxVx4[$HJ))cm}kL0Vg*uT9+?4]GXtx=7WVZ<Ol8RPQQ6,^2pP=z$&QuwrUk.+}}RxV',
+		20 => 'Kh6I():h#dp8zWm}.vF:xQkL`sg?baRK=Z[^|:;-ObR.9#9%uJ 5K]sl/DU@sII}Kh6I():h#dp8zWm}.vF:xQkL`sg?baRK=Z[^|:;-ObR.9#9%uJ 5K]sl/DU@sII}'
+	];
+
+?>
