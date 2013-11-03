f2833bdc84000a3165cc881317d1e814094d9ec7
Vegar Norman <vegar@theinternetloves.me>
2013-11-03 20:28:38 +0100

    Opprettet User.auth.class.php
    Klasse som lager et UserAuth-objekt som brukes til hashing av passord.

---
+++ application/classes/User.auth.class.php
@@ -0,0 +1,36 @@
+<?php
+
+	require "User.settings.php";
+
+	class UserAuth {
+
+		private $user;
+		private $hash;
+
+		public function __construct($u, $p) {
+			$this->user = $u;
+			$this->hash = $this->makeHash($u, $p);
+		}
+
+		public function makeHash($u, $p) {
+			global $s;
+			$ul = strlen($u);
+			$u = md5($u);
+			$p = md5($p);
+			$su = str_split($u, ((strlen($u) / 2) + 4));
+			$p = str_split($p, ((strlen($p) / 2) + 7));
+			$p = (string) base64_encode(hash_hmac("sha512", $su[0] . $s[0] . hash("whirlpool", base64_encode((string) $su[0] . $p[0] . $s[$ul] . $su[1] . $p[1]) . $s[0]) . $su[1], $s[0]));
+			return $p;
+		}
+
+		public function getUser() {
+			return $this->user;
+		}
+
+		public function getHash() {
+			return $this->hash;
+		}
+
+	}
+
+?>
