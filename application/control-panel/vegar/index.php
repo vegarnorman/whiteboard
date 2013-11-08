d908c0957e23df53b1832dfc961105ece2a25781
Vegar Norman <vegar@theinternetloves.me>
2013-11-08 23:12:23 +0100

    Initial
    Vegars kode for kontrollpanelet

---
+++ application/control-panel/vegar/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+<html lang="no">
+
+<head>
+	<meta charset="utf-8" />
+
+	<link rel="stylesheet" href="css/master.css" />
+	<script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+	<script src="js/main.js"></script>
+
+	<title>Kontrollpanel</title>
+</head>
+
+<body>
+
+	<div class="cp-container">
+
+		<aside class="cp-sidebar">
+
+		</aside>
+
+
+		<section class="cp-main">
+
+			<h2 class="grid g12 cp-site-title">Vår fantastiske nettside</h2>
+
+		</section>
+
+	</div>
+
+</body>
+</html>
