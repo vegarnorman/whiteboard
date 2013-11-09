f4a6391332212da35c0535859543d34b8630f96f
NothingButTheHoff <pererikfinstad@pereriktadsmbp2.lan>
2013-11-09 12:31:03 +0100

    Lagd undersider og dynamisk meny (php include)

---
+++ application/demoside/includes/forside.php
@@ -0,0 +1,14 @@
+<h2>Forside</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet, felis nec imperdiet dapibus, lorem mi vehicula
+		 	massa, vel pellentesque ipsum est vel quam. Donec vestibulum et lectus eget fermentum. Ut at nisl volutpat, ullamcorper lacus
+		  	vitae, hendrerit leo. Praesent a lectus augue. In a elit eros. In rutrum diam ante, ut scelerisque lectus tincidunt eget. 
+		  	Cras nec neque tincidunt ante sagittis rhoncus.
+		</p>
+
+		<p>
+			Cras malesuada, lorem in malesuada placerat, risus magna aliquet orci, quis fringilla dui magna id nibh. 
+			Aliquam leo lectus, aliquet et ultrices et, luctus sit amet lorem. Sed ac suscipit justo. 
+			Nulla sollicitudin elementum luctus. Aliquam erat volutpat. Donec a adipiscing lacus, ut suscipit quam. 
+			Nullam suscipit arcu a accumsan condimentum. In et elit ac lorem consequat blandit fringilla nec magna.
+		</p>
--- application/demoside/index.php
+++ application/demoside/index.php
@@ -16,29 +16,35 @@
 	<header>
 		<nav>
 			<ul>
-				<li><a href="#">Example1</a></li>
-				<li><a href="asød#">Example2</a></li>
-				<li><a href="#asd">Example3</a></li>
-				<li><a href="asd#">Example4</a></li>
-				<li><a href="asd#">Example5</a></li> 
+				<li><a href="index.php?page=forside">Forside</a></li>
+				<li><a href="index.php?page=blogg">Blogg</a></li>
+				<li><a href="index.php?page=artikler">Artikler</a></li>
+				<li><a href="index.php?page=mer">Mer</a></li>
+				<li><a href="index.php?page=kontakt">Kontakt</a></li> 
 			</ul>
 		</nav >
 	</header>
 	<div class="main-container">
-		<h2>Contents</h2>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet, felis nec imperdiet dapibus, lorem mi vehicula
-		 	massa, vel pellentesque ipsum est vel quam. Donec vestibulum et lectus eget fermentum. Ut at nisl volutpat, ullamcorper lacus
-		  	vitae, hendrerit leo. Praesent a lectus augue. In a elit eros. In rutrum diam ante, ut scelerisque lectus tincidunt eget. 
-		  	Cras nec neque tincidunt ante sagittis rhoncus.
-		</p>
-
-		<p>
-			Cras malesuada, lorem in malesuada placerat, risus magna aliquet orci, quis fringilla dui magna id nibh. 
-			Aliquam leo lectus, aliquet et ultrices et, luctus sit amet lorem. Sed ac suscipit justo. 
-			Nulla sollicitudin elementum luctus. Aliquam erat volutpat. Donec a adipiscing lacus, ut suscipit quam. 
-			Nullam suscipit arcu a accumsan condimentum. In et elit ac lorem consequat blandit fringilla nec magna.
-		</p>
+		<?php 
+				if (isset($_GET['page'])) {
+					switch($_GET['page']){
+						case 'forside':
+						case 'blogg':
+						case 'artikler':
+						case 'mer': 
+						case 'kontakt':
+						    include("includes/$_GET[page].php");
+						break;
+					
+						default: 
+						    include('includes/forside.php');
+						break;
+					}
+				}
+				else{
+					include('includes/forside.php');
+				}
+		?>
 	</div>
 
 	<footer>
