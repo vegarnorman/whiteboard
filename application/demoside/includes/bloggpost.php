296837421d7088f3011e51f9169d8952a4f4d474
M4R1US <baltr.marius@gmail.com>
2013-11-10 19:12:16 +0100

    bloggpost.php
    php filen av bloggpost

---
+++ application/demoside/includes/bloggpost.php
@@ -0,0 +1,18 @@
+<div class="container">
+	<section class="grid g12 no-gutters">
+			<form action="#" name ="kommenter">
+					<h2>Kommentarside</h2>				
+					<h4>Navn</h4>
+					<input type="text" name="navn" value="navn">
+					<h4>Etternavn</h4>
+					<input type="text" name="etternavn" value="etternavn">
+					<h4>E-post</h4>
+					<input type="text" name="etternavn" value="epost">
+					<h3>Skriv Kommentar</h3>
+					<textarea rows="4" cols="50">
+					</textarea><br>
+					<input type="submit" value="Submit">
+			</form>
+	</section>
+</div>
+
