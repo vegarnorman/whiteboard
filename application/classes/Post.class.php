3ec6c3db8fe0a65150dcd9c556366cdf5e533349
Vegar Norman <vegar@theinternetloves.me>
2013-11-12 10:41:17 +0100

    Bugfixes
    Bugfixes relatert til resultatobjekter, tilkobling og spørringer.

--- application/classes/Post.class.php
+++ application/classes/Post.class.php
@@ -14,8 +14,16 @@
 
 
 		public function __construct($data) {
-			if (exists($data["post_id"])) {
+			if (isset($data["post_id"])) {
 				$this->post_id = $data["post_id"];
+			}
+
+			if (isset($data["user_display_name"])) {
+				$this->user_display_name = $data["user_display_name"];
+			}
+
+			if (isset($data["user_display_name"])) {
+				$this->user_last_modified_by_display_name = $data["user_last_modified_by_display_name"];
 			}
 
 			$this->post_title = $data["post_title"];
@@ -24,8 +32,7 @@
 			$this->post_last_modified = $data["post_last_modified"];
 			$this->post_published_by = $data["post_published_by"];
 			$this->post_last_modified_by = $data["post_last_modified_by"];
-			$this->user_display_name = $data["user_display_name"];
-			$this->user_last_modified_by_display_name = $data["user_last_modified_by_display_name"];
+			
 		}
 
 
@@ -54,7 +61,6 @@
 
 			else if ($step1 == 0) {
 				$handle->rollback();
-				$step1->free();
 				return (int) 0;
 			}
 
@@ -67,13 +73,11 @@
 
 				if (!$step2) {
 					$handle->rollback();
-					$step2->free();
 					return false;
 				}
 
 				else if ($step2 == 0) {
 					$handle->rollback();
-					$step2->free();
 					return (int) 0;
 				}
 
@@ -82,25 +86,21 @@
 					$post["post_last_modified"] = $step2["post_last_modified"];
 					$post["post_published_by"] = $step2["post_published_by"];
 					$post["post_last_modified_by"] = $step2["post_last_modified_by"];
-					$step2->clear();
 
 					$step3 = $handle->operation("get", "Profile", ["user_id" => $post["post_published_by"]]);
 
 					if (!$step3) {
 						$handle->rollback();
-						$step3->free();
 						return false;
 					}
 
 					else if ($step3 == 0) {
 						$handle->rollback();
-						$step3->free();
 						return (int) 0;
 					}
 
 					else {
 						$post["user_display_name"] = $step3["user_display_name"];
-						$step3->free();
 
 						$step4 = $handle->operation("get", "Profile", ["user_id" => $post["post_last_modified_by"]]);
 
@@ -111,13 +111,11 @@
 
 						else if ($step4 == 0) {
 							$handle->rollback();
-							$step4->free();
 							return (int) 0;
 						}
 
 						else {
 							$post["user_last_modified_by_display_name"] = $step4["user_display_name"];
-							$step4->free();
 							$handle->commit();
 
 							return $post;
@@ -133,42 +131,43 @@
 			$handle->begin();
 
 			$step1_data = [
-				"post_title" => $this->post_title,
-				"post_data" =>  $this->post_data
+				"post_title" => (string) $this->post_title,
+				"post_data" =>  (string) $this->post_data
 			];
 
 			$step1 = $handle->operation("insert", "Post", $step1_data);
 
 			if (!$step1) {
 				$handle->rollback();
-				$step1->free();
 				return false;
 			}
 
 			else {
+
+				$this->post_id = $step1;
+
 				$step2_data = [
-					"post_id" =>  $step1,
-					"post_published" =>  $this->post_published,
-					"post_last_modified" =>  $this->post_last_modified,
-					"post_published_by" =>  $this->post_published_by,
-					"post_last_modified_by" =>  $this->post_last_modified_by
+					"post_id" =>  (int) $this->post_id,
+					"post_published" => (string) $this->post_published,
+					"post_last_modified" => (string) $this->post_last_modified,
+					"post_published_by" => (int) $this->post_published_by,
+					"post_last_modified_by" => (int) $this->post_last_modified_by
 				];
-				$step1->free();
 
 				$step2 = $handle->operation("insert", "Post_Meta", $step2_data);
 
 				if (!$step2) {
 					$handle->rollback();
-					$step2->free();
 					return false;
 				}
 
 				else {
 					$handle->commit();
-					$step2->free();
 					return true;
 				}
+
 			}
+			
 		}
 
 
@@ -180,13 +179,11 @@
 
 			if (!$step1) {
 				$handle->rollback();
-				$step1->free();
 				return false;
 			}
 
 			else {
 				$handle->commit();
-				$step1->free();
 				return true;
 			}
 		}
@@ -206,18 +203,15 @@
 
 			if (!$step1) {
 				$handle->rollback();
-				$step1->free();
 				return false;
 			}
 
 			else if ($step1 == 0) {
 				$handle->rollback();
-				$step1->free();
 				return (int) 0;
 			}
 
 			else {
-				$step1->free();
 				$step2_data = [
 					"post_id" => $post_id,
 					"post_published" => $post_published,
@@ -230,25 +224,20 @@
 
 				if (!$step2) {
 					$handle->rollback();
-					$step2->free();
 					return false;
 				}
 
 				else if ($step2 == 0) {
 					$handle->rollback();
-					$step2->free();
 					return (int) 0;
 				}
 
 				else {
 					$handle->commit();
-					$step2->free();
 					return true;
 				}
 			}
 		}
-
-
 	}
 
  ?>
