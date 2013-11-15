566d04386c401337e079b32b44a12b61f8c4a370
Vegar Norman <vegar@theinternetloves.me>
2013-11-15 21:40:00 +0100

    Oppdaterte databasedetaljer
    Ikke endre disse detaljene! Dersom du ønsker å operere mot lokal
    database må du ikke committe denne filen etter du har endret detaljene!

--- application/classes/Database.settings.php
+++ application/classes/Database.settings.php
@@ -1,7 +1,6 @@
 <?php
-	$db0 = '_RYy9ll{jk4V2m9v}c>9^?ejl=.tO!{XVnXh3WfdIBE.`#x[0p3jqKi6JSD2{uWL';
-	$db1 = 'bWrB+0cOEKhVJpC5fWGUAXHnmBEEqoWCtQqfHqnMR+0=';
-	$db2 = 'QE7IKoU1Pk7/gkigSo3FTmZIZ2miR5vvWjSNIRTgPD8=';
-	$db3 = 'yOrCnl26nvyMkMhDYq6T54KmSyYDSZd3O3134O6F4Uc=';
-	$db4 = '4DaG/I4htCR0tyPLEtfOTIXRL9cpdW73g9/Ys4uaKtc=';
+	$db1 = "vegar.me";
+	$db2 = "webprosjekt3";
+	$db3 = "Pilestredet35";
+	$db4 = "wpg25";
 ?>
