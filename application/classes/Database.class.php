26a898e81d205a45a613d18939084fa88a7d8b08
Vegar Norman <vegar@theinternetloves.me>
2013-11-15 21:39:19 +0100

    Fjernet krypteringsfunksjonalitet
    Krypteringen av databasetilkoblingsdetaljer var avhengig av et eksternt
    tillegg til PHP, og funksjonen er foreløpig fjernet til en eventuell
    fiks forekommer.

--- application/classes/Database.class.php
+++ application/classes/Database.class.php
@@ -7,11 +7,11 @@
 
 		public function __construct() {
 			global $db0, $db1, $db2, $db3, $db4;
-			$auth = new DatabaseAuth($db0);
-			$host = $auth->decrypt($db1);
-			$user = $auth->decrypt($db2);
-			$pass = $auth->decrypt($db3);
-			$use  = $auth->decrypt($db4);
+			// $auth = new DatabaseAuth($db0);
+			$host = $db1;
+			$user = $db2;
+			$pass = $db3;
+			$use  = $db4;
 
 			parent::__construct($host, $user, $pass, $use);
 			$this->set_charset("utf8");
