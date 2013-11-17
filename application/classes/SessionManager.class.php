2bfdfd810679fc839425e8ed18d6de3779bfe213
Vegar Norman <vegar@theinternetloves.me>
2013-11-17 13:24:17 +0100

    Implementert funksjonalitet

--- application/classes/SessionManager.class.php
+++ application/classes/SessionManager.class.php
@@ -6,6 +6,26 @@
 
 		}
 
+		public static function setSession($id) {
+			$s1 = decoct(($id + 34850339) - 1);
+			$_SESSION["session_user"] = $s1;
+		}
+
+		public static function checkSession() {
+			if (!isset($_SESSION["session_user"])) {
+				return false;
+			}
+
+			else {
+				return true;
+			}
+		}
+
+		public static function getUserID() {
+			$id = octdec($_SESSION["session_user"]);
+			echo ($id + 1) - 34850339;
+		}
+
 	}
 
  ?>
