0b44490220466b4e7bc8b5c0bbefec5da92c1388
Vegar Norman <vegar@theinternetloves.me>
2013-11-20 21:19:40 +0100

    Laget Javascript for filedrop

---
+++ application/control-panel/js/filedrop.js
@@ -0,0 +1,48 @@
+$(function() {
+
+		var fileContent = "";
+
+		function filedropDragover(evt) {
+			evt.stopPropagation();
+			evt.preventDefault();
+			evt.dataTransfer.dropEffect = "copy";
+		}		
+
+		function filedropDropAction(evt) {
+			evt.stopPropagation();
+			evt.preventDefault();
+
+			var files = evt.dataTransfer.files;
+
+			for (var i = 0; i < files.length; i++) {
+				var file = files[i];
+				var reader = new FileReader();
+
+				reader.onload = (function(theFile) {
+					return function(e) {
+						fileContent = '{ "post_data": "' +  e.target.result +  '" }';
+						fileContent = fileContent.replace(/\n/g, '<br />');
+						form = document.createElement("form");
+						form.setAttribute("method", "post");
+						form.setAttribute("action", "editor.php");
+						content = document.createElement("input");
+						content.setAttribute("name", "filedrop_content");
+						content.setAttribute("id", "filedrop_content");
+						content.setAttribute("value", fileContent);
+						form.appendChild(content);
+						document.body.appendChild(form);
+						form.submit();
+					};
+				})(file);
+
+				reader.readAsText(file);
+			}
+		}
+	
+
+		var filedrop = document.getElementById("cp-filedrop");
+		filedrop.addEventListener("dragover", filedropDragover, false);
+		filedrop.addEventListener("drop", filedropDropAction, false);
+		filedrop.addEventListener("change", filedropDropAction, false);
+
+});
