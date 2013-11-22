ec0b5e2e1d689ec64bbd2fd91e4e1fcef80b3bd0
M4R1US <baltr.marius@gmail.com>
2013-11-22 11:13:07 +0100

    Noen javascript funksjoner for easter egg
    Noen javascript funksjoner for easter egg

--- themes/default/js/functions.js
+++ themes/default/js/functions.js
@@ -13,6 +13,27 @@ window.onload = function highlight(){
 	}
 };
 
+// Easter Egg
+
+var navnomraade = document.getElementsByTagName("input");
+var chucknavn = navnomraade.getAttribute("placeholder");
+
+
+function popupwindow(url, title, w, h) {
+  var left = (screen.width/2)-(w/2);
+  var top = (screen.height/2)-(h/2);
+  return window.open(url="http://www.empireonline.com/images/uploaded/chuck-norris-uzis.jpg", title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+} 
+
+ function vis() {
+	if (navnomraade[0].value === "Chuck Norris") {
+	popupwindow();
+	}
+}
+//
+
+
+
 
 // //hide comment form on load
 document.ready = function hideDiv(){
