074533c96c5c45b144e0f634f83775809026f3fa
Vegar Norman <vegar@theinternetloves.me>
2013-11-22 09:23:53 +0100

    Fikset validering i kategorinavn

--- application/control-panel/js/view.js
+++ application/control-panel/js/view.js
@@ -1,3 +1,8 @@
 $(function() {
- 
+	$("#new_category #submit").click(function() {
+		if ($("#new_category_name").val() === "") {
+			alert("Du må skrive inn et kategorinavn. Vennligst forsøk igjen.");
+			return false;
+		}
+	});
 });
